4d1d0fdfb539f094e9de906824d3db50a12c5b98
jose carlos <carlos.souzamarinho1976@gmail.com>
2026-06-18 22:55:58 -0300

    integrando a API

---
+++ projetc.py
@@ -0,0 +1,7 @@
+import requests
+from supabase import create_client, Client 
+
+SUPABASE_URL = "https://glgskjpjzybttiqcwymd.supabase.co/rest/v1/"
+SUPABASE_KEY = "sb_publishable_nYK9W_uyBaf5CwOhNJ0jwA_3jxI6zwP"
+
+print("Connecting to Supabase...")
